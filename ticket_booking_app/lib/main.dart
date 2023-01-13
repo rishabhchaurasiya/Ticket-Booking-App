4fbd9cd2e2556d228e624c226dbda5de36808ed5
RISHABH <rishabhchaurasiya20899@gmail.com>
2023-01-13 13:14:50 +0530

    done

--- ticket_booking_app/lib/main.dart
+++ ticket_booking_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar(),
     );
